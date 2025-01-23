099d2b9b927f114a1bece1be055a273530dd3711
Astitwa2424 <darsanme49@gmail.com>
2025-01-23 19:55:17 +1100

    Sidebar

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,7 +1,7 @@
 import { Button, type ButtonProps } from '@/components/ui/button'
 import { cn } from '@/utilities/ui'
 import Link from 'next/link'
-import React from 'react'
+import type React from 'react'
 
 import type { Page, Post } from '@/payload-types'
 
@@ -15,7 +15,7 @@ type CMSLinkType = {
     relationTo: 'pages' | 'posts'
     value: Page | Post | string | number
   } | null
-  size?: ButtonProps['size'] | null
+  size?: ButtonProps['size'] | 'clear' | null
   type?: 'custom' | 'reference' | null
   url?: string | null
 }
@@ -35,17 +35,14 @@ export const CMSLink: React.FC<CMSLinkType> = (props) => {
 
   const href =
     type === 'reference' && typeof reference?.value === 'object' && reference.value.slug
-      ? `${reference?.relationTo !== 'pages' ? `/${reference?.relationTo}` : ''}/${
-          reference.value.slug
-        }`
+      ? `${reference?.relationTo !== 'pages' ? `/${reference?.relationTo}` : ''}/${reference.value.slug}`
       : url
 
   if (!href) return null
 
-  const size = appearance === 'link' ? 'clear' : sizeFromProps
+  const size = appearance === 'link' ? undefined : sizeFromProps
   const newTabProps = newTab ? { rel: 'noopener noreferrer', target: '_blank' } : {}
 
-  /* Ensure we don't break any styles set by richText */
   if (appearance === 'inline') {
     return (
       <Link className={cn(className)} href={href || url || ''} {...newTabProps}>
@@ -56,8 +53,13 @@ export const CMSLink: React.FC<CMSLinkType> = (props) => {
   }
 
   return (
-    <Button asChild className={className} size={size} variant={appearance}>
-      <Link className={cn(className)} href={href || url || ''} {...newTabProps}>
+    <Button
+      asChild
+      className={className}
+      size={size === 'clear' ? undefined : size}
+      variant={appearance === 'link' ? 'link' : appearance}
+    >
+      <Link href={href || url || ''} {...newTabProps}>
         {label && label}
         {children && children}
       </Link>
